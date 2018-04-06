ea644da3fab3f1400d940b6b5d93d8c9b322be77
高昇 <sheng.gao03@ele.me>
2018-04-06 15:01:33 +0800

    maven init

---
+++ src/main/java/entity/one.java
@@ -0,0 +1,8 @@
+package entity;
+
+/**
+ * Create by Ever_Gao on 2018/4/6
+ */
+
+public class one {
+}
---
+++ src/test/java/one/test.java
@@ -0,0 +1,8 @@
+package one;
+
+/**
+ * Create by Ever_Gao on 2018/4/6
+ */
+
+public class test {
+}
